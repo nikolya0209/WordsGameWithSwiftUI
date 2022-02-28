2e89411abc931216d78f9e309bd8bfcd42671b4c
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-02-28 19:02:12 +0300

    Initial Commit

---
+++ WordsGameWithSwiftUI/WordsGameWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameWithSwiftUIApp.swift
+//  WordsGameWithSwiftUI
+//
+//  Created by MacBookPro on 28.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
